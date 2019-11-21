59967ce492fccd314ba7fb52e32fb003a057dfec
张无忌 <1828405694@qq.com>
2019-11-21 20:17:42 +0800

    练习题

---
+++ test_11.21/test_11.21/11.21.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int a, b;
+//	int i;
+//	scanf("%d %d", &a, &b);
+//	int min = a;
+//	if (a > b)
+//	{
+//		min = b;
+//	}
+//	for (i = min; i > 0; i--)
+//	{
+//		if ((a%i == 0) && (b%i == 0))
+//		{
+//			printf("%d \n", i);
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a, b;
+//	int c = 0;
+//	scanf("%d %d", &a, &b);
+//	while(c = a % b)
+//	{
+//		a = b;
+//		b = c;
+//	} 
+//	printf("%d \n", b);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int i=0;
+//	int count = 0;
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1,&n2);
+//	for (i = n1; i <= n2; i++)
+//	{
+//		int t = 0;
+//		t = i;
+//		while (t > 0)
+//		{
+//			if ((t % 10) == 9)
+//				count++;
+//				t = t / 10;
+//		}
+//	}
+//	printf("%d \n", count);
+//	system("pause");
+//	return 0;
+//}
