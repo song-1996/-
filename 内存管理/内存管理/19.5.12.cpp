d24dd23f28d66490b370bd5aae846ce96d181299
张无忌 <1828405694@qq.com>
2020-03-01 13:19:15 +0800

    内存管理demo

---
+++ 内存管理/内存管理/19.5.12.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	printf("%d\n", sizeof(nullptr));
+	system("pause");
+	return 0;
+
+}
